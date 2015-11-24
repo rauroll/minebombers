7352214204e2a7f2575d36f6d4534db47455a089
Henri Nurmi <henri.nurmi@kapsi.fi>
2015-11-24 16:27:38 +0200

    Removed object files

--- src/minebombers/build/Debug/GNU-MacOSX/Game.o.d
+++
@@ -1,5 +0,0 @@
-build/Debug/GNU-MacOSX/Game.o: Game.cpp Game.h Scene.h
-
-Game.h:
-
-Scene.h:
--- src/minebombers/build/Debug/GNU-MacOSX/main.o.d
+++
@@ -1,7 +0,0 @@
-build/Debug/GNU-MacOSX/main.o: main.cpp Game.h Scene.h MenuScene.h
-
-Game.h:
-
-Scene.h:
-
-MenuScene.h:
