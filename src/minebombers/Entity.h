e99450088668908359b08dd467f814d7a2e02f9b
Jere Vaara <jerevaara@gmail.com>
2015-12-02 13:55:04 +0200

    entity move method

--- src/minebombers/Entity.h
+++ src/minebombers/Entity.h
@@ -27,6 +27,7 @@ public:
     const sf::Sprite& getSprite() const; 
     const std::tuple<int, int> getPos() const;
     void setPos(int x, int y);
+    void move(sf::Vector2u dir);
     
     std::string getName() const;
 protected:
