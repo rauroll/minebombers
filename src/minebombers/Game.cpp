fa450bd95dc03227a27f81c179019668ee737eab
Max Huttunen <max.huttunen@gmail.com>
2015-12-08 21:20:01 +0200

    random player spawning

--- src/minebombers/Game.cpp
+++ src/minebombers/Game.cpp
@@ -23,8 +23,10 @@
 
 #include <iostream>
 #include <cstdio>
+#include <time.h>
 
 Game::Game() {
+    srand(time(NULL));
     MapGenerator gen = MapGenerator();
     MapLoader loader = MapLoader();
     
@@ -116,7 +118,15 @@ void Game::movePlayer(uint8_t player, sf::Vector2u d) {
 }
 
 void Game::addPlayer(const std::string& name) {
-    sf::Vector2u pos(rand() % 20, rand() % 20);
+    sf::Vector2u pos;
+    sf::Vector2u mapSize = map.getSize();
+    while (true) {
+        pos = sf::Vector2u(rand() % mapSize.x, rand() % mapSize.y);
+        if (map.canMoveTo(pos) && !this->isEntityAtPos(pos))
+            break;
+    }
+    
+    std::cout << "asd: " << pos.x << "," << pos.y << std::endl;
     
     Player p("assets/playersprite.png", pos.x, pos.y, name);
     players.push_back(p);
